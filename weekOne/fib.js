219c177c088ecc975b1e2518c34f571da209909c
dkang417 <dkang417@gmail.com>
2018-06-16 20:04:51 -0400

    add fibonacci

---
+++ weekOne/fib.js
@@ -0,0 +1,21 @@
+
+function fib(){
+	let prev = 0;
+	let curr = 1;
+
+	function nacci(){
+		const temp = prev;
+		console.log(curr);
+		prev = curr;
+		curr = curr + temp;
+	}
+	return nacci;
+}
+
+var fibCounter = fib();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
+fibCounter();
