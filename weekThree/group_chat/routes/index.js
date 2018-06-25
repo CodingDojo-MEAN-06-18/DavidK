3169a727e959d4b79d7ff77ee7c985fa4ce1f038
dkang417 <dkang417@gmail.com>
2018-06-25 10:17:59 -0400

    added groupchat

---
+++ weekThree/group_chat/routes/index.js
@@ -0,0 +1,13 @@
+module.exports = function Route(app, server){
+	//this gets the socket io module 
+	
+	var io = require('socket.io').listen(server)
+	// root route to render the index.ejs view
+	app.get('/', function(req, res) {
+		res.render("index");
+	});
+
+	
+}
+
+
