fc918cad482b5ba0b55ad98a6950a1d804391708
dkang417 <dkang417@gmail.com>
2018-07-02 23:03:08 -0400

    modularization

--- weekFour/Quoting_Dojo/server.js
+++ weekFour/Quoting_Dojo/server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('express-flash');
 const path = require('path');
-const mongoose = require('mongoose'),
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
@@ -18,8 +17,8 @@ app.use(session({
 }));
 
 
-require('./server/config/routes.js')(app);
 require('./server/config/mongoose.js');
+require('./server/config/routes.js')(app);
 
 app.listen(8000, function() {
     console.log("listening on port 8000");
