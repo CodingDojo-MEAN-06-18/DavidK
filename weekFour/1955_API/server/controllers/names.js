31bac7c5c218a52f9aea290e29b4894489dbd28f
dkang417 <dkang417@gmail.com>
2018-07-07 09:44:11 -0400

    update controller 1955

--- weekFour/1955_API/server/controllers/names.js
+++ weekFour/1955_API/server/controllers/names.js
@@ -3,34 +3,31 @@ const Name = require("mongoose").model("Name");
 
 module.exports = {
 
-    index(req, res){
-         	Name.find({})
-            .then(name => res.json(name))
-            .catch(error => res.json(error));
-    },
+  index(req, res){
+    Name.find({})
+      .then(name => res.json(name))
+      .catch(error => res.json(error));
+  },
 
-    show(req,res){
-	    Name.findOne(req.params)
-        .then( name => {
-            res.json(name ? name: 'no such person existed in 1955');
-        })
-        .catch( error => res.json(error));
-        
-    },
+  show(req,res){
+	 Name.findOne(req.params)
+    .then( name => {
+      res.json(name ? name: 'no such person existed in 1955');
+    })
+    .catch( error => res.json(error));   
+  },
 
-    create(req, res) {
-    	Name.create(req.params)
-        .then( name => res.json(name))
-        .catch( error => res.json(error));
-    },
-	 
-
-	 destroy(req,res) {
-		  Name.remove(req.params)
-        .then( name => res.json(name))
-        .catch( error => res.json(error));
-    },
+  create(req, res) {
+    Name.create(req.params)
+      .then( name => res.json(name))
+      .catch( error => res.json(error));
+  },
+	
+  destroy(req,res) {
+    Name.remove(req.params)
+      .then( name => res.json(name))
+      .catch( error => res.json(error));
+  },
     
-
 
 };
