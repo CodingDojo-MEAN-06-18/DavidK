07e38bdbd080976ed71a4fe3bdd4cab2830916ab
dkang417 <dkang417@gmail.com>
2018-07-10 22:07:37 -0400

    added switchboard

---
+++ angular/SwitchBoardApp/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: String = 'Switchboard';
+  
+  switches = [true,true,true,true,true,true,true,true,true,true];
+  
+
+  flipSwitch(i){
+    this.switches[i] = !this.switches[i];
+  }
+ 
+}
+
