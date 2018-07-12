23d12d1dad40bfbd6b8a43582ced78ab26861866
dkang417 <dkang417@gmail.com>
2018-07-11 22:37:39 -0400

    updated registration with validations

--- angular/Registration/src/app/app.component.ts
+++ angular/Registration/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: String = 'Registration';
+
+  user = new User();
+  users: Array<User> = [];
+
+  onSubmit(event: Event, form: NgForm) {
+    event.preventDefault();
+    console.log('submitting form and preventing default', this.user);
+    this.users.push(this.user);
+    this.user = new User();
+
+    form.reset();
+
+    console.log('users', this.users);
+  }
 
 }
---
+++ angular/Registration/src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirm: string;
+  address: string;
+  unit: string;
+  city: string;
+  state: string;
+  lucky: string;
+}
