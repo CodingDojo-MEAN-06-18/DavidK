0174b746cd8134d5c7c03139d03fae0a329233df
dkang417 <dkang417@gmail.com>
2018-08-03 16:01:29 -0400

    set all the components

--- fullMean/teammanagers/src/app/app.module.ts
+++ fullMean/teammanagers/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { PlayersService } from './teammanager/services/team/players.service';
+
 import { TeammanagerComponent } from './teammanager/teammanager.component';
 import { GameComponent } from './teammanager/games/game/game.component';
 import { AddplayerComponent } from './teammanager/players/addplayer/addplayer.component';
 import { ListplayersComponent } from './teammanager/players/listplayers/listplayers.component';
-import { PlayersComponent } from './teammanager/services/players/players.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,13 @@ import { PlayersComponent } from './teammanager/services/players/players.compone
     GameComponent,
     AddplayerComponent,
     ListplayersComponent,
-    PlayersComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
