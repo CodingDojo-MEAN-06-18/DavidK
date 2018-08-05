2653c83533ea11edeb78d842ee0f8c5ca28bbbcb
dkang417 <dkang417@gmail.com>
2018-08-05 16:23:30 -0400

    need to work on serverside for githubbattle

--- fullMean/githubbattle/src/app/github/battle/faceoff/faceoff.component.ts
+++ fullMean/githubbattle/src/app/github/battle/faceoff/faceoff.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../../../services/github.service';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-faceoff',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaceoffComponent implements OnInit {
 
-  constructor() { }
+  user1: User = new User();
+  user2: User = new User();
+
+  constructor(private readonly _gitHub: GithubService, private readonly _route: Router) { }
 
   ngOnInit() {
+  }
+
+  setUser(event: Event, player: number): void {
+    event.preventDefault();
+    const user = player === 1 ? this.user1 : this.user2;
+    this._gitHub.getUser(user.name).subscribe(data => {
+      user.name = data['login'];
+      user.image = data['avatar_url'];
+      user.score = (data['followers'] + data['public_repos']) * 12;
+    });
+  }
+
+  battle(): void {
+    this._gitHub.battle(this.user1, this.user2);
+    this._route.navigate(['github', 'battle', 'winner']);
   }
 
 }
--- fullMean/githubbattle/src/app/github/battle/winner/winner.component.ts
+++ fullMean/githubbattle/src/app/github/battle/winner/winner.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../../../services/github.service';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-winner',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WinnerComponent implements OnInit {
 
-  constructor() { }
+  winner: User = this._gitHub.winner;
+  loser: User = this._gitHub.loser;
+
+  constructor(private readonly _gitHub: GithubService, private readonly _route: Router) { }
 
   ngOnInit() {
+  }
+  reset(): void {
+    this._gitHub.winner = this._gitHub.loser = new User();
+    this._route.navigate(['/']);
   }
 
 }
